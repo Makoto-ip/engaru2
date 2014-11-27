ceecee40a3c24bed8110245cb52b1f07a7396e85
Makoto Takahashi <TakahashiMakoto@Makoto-no-Mac-mini.local>
2014-11-27 13:26:52 +0900

    Initial Commit

---
+++ engarutyou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  engarutyou
+//
+//  Created by Makoto Takahashi on 2014/11/27.
+//  Copyright (c) 2014年 com.domainname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ engarutyou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  engarutyou
+//
+//  Created by Makoto Takahashi on 2014/11/27.
+//  Copyright (c) 2014年 com.domainname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
